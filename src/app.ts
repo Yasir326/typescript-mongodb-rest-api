8521c5c24cd011bff8de7902010307324e174120
Yasir Khan <yasir.khan@zoopla.co.uk>
2021-12-08 22:25:07 +0000

    add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import config from "config";
 import connection from "./utils/connection";
-import logger from './utils/logger';
+import logger from "./utils/logger";
+import routes from "./routes";
 
 const port = config.get<number>("port");
 const app = express();
 
 app.listen(port, async () => {
-    logger.info("Listening on port: ", port);
+  logger.info("Listening on port: ", port);
   await connection();
+
+  routes(app);
 });
