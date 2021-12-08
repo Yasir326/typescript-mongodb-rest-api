9d181dcac8f00c9cf55b576ccc49343c2fd380ca
Yasir Khan <yasir.khan@zoopla.co.uk>
2021-12-08 21:58:38 +0000

    add logger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import config from "config";
 import connection from "./utils/connection";
+import logger from './utils/logger';
 
 const port = config.get<number>("port");
 const app = express();
 
 app.listen(port, async () => {
-  console.log("Listening on port: ", port);
-
+    logger.info("Listening on port: ", port);
   await connection();
 });
--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import config from "config";
+import logger from './logger'
 
 async function connection() {
   const dbUri = config.get<string>("dbUri");
   try {
     await mongoose.connect(dbUri);
-    console.log("Connected to the DB");
+    logger.info("Connected to the DB");
   } catch (error) {
-    console.error("Could not connect to the DB due to", error);
+    logger.error("Could not connect to the DB due to", error);
     process.exit(1);
   }
 }
