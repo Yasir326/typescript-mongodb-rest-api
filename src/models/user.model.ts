b5b99c596d3a6d7c591ce661a3077ecb1bf9385c
Yasir Khan <yasir.khan@zoopla.co.uk>
2021-12-09 17:34:02 +0000

    Add user.model.ts and bcrypt hashing

---
+++ src/models/user.model.ts
@@ -0,0 +1,42 @@
+import { Schema, model, Document } from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
+import { string } from "zod";
+
+export interface UserSchema extends Document {
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema(
+  {
+    email: { type: string, required: true, unique: true },
+    name: { type: string, required: true },
+    password: { type: string, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  let user = this as UserSchema;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(config.get<number>("saltRounds"));
+  const hash = await bcrypt.hash(user.password, salt);
+
+  user.password = hash;
+
+  return next();
+});
+
+const UserModel = model("User", userSchema);
+
+export default UserModel;
